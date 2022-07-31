44fb9a54cf622d30385f7ca0e882b97588daf062
KeSamanthaChen <ke.samantha.chen@tum.de>
2022-07-31 20:23:11 +0000

    test task8.2

--- nix-build-shell.cpp
+++ nix-build-shell.cpp
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/69153764/unshare-user-namespace-fork-map-uid-then-execvp-failing
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -6,12 +7,12 @@
 #include <stdlib.h> // execvp
 #include <unistd.h> // fork, exec
 #include <sys/wait.h> // waitpid
+#include <sched.h> //unshare
 
 int main(int argc, const char** argv) {
     // std::cout << "Hello from " << argv[0] << ". I got " << argc << " arguments\n" << std::endl;
 
     // argv[0] argv[1]->build_dir argv[2] ... (command)
-
     std::stringstream file_path_ss;
     file_path_ss << argv[1] << "/env-vars";
     std::string file_path_string = file_path_ss.str();
@@ -50,9 +51,48 @@ int main(int argc, const char** argv) {
 
     fprintf(stderr, "test argument: %s\n", exe_argv[2]);
 
+    // getuid, getgid
+    int pid = getpid();
+    uid_t uid = getuid();
+    gid_t gid = getgid();
+    // unshare
+    if (0!=unshare(CLONE_NEWUSER)) {
+        fprintf(stderr, "create new user namespace failed\n");
+        exit(1);
+    }
+    std::string uid_file_path;
+    std::string gid_file_path;
+    std::string setgroups_file_path;
+    uid_file_path.append("/proc/").append(std::to_string(pid)).append("/uid_map");
+    gid_file_path.append("/proc/").append(std::to_string(pid)).append("/gid_map");
+    setgroups_file_path.append("/proc/").append(std::to_string(pid)).append("/setgroups");
+
+    std::ofstream uid_file(uid_file_path);
+    std::ofstream gid_file(gid_file_path);
+    std::ofstream setgroups_file(setgroups_file_path);
+
+    if (uid_file.is_open()) {
+        uid_file << "1000" << " " << uid << " 1" << std::endl;
+        uid_file.close();
+    }
+
+    if (setgroups_file.is_open()) {
+        setgroups_file << "deny" << std::flush;
+        setgroups_file.close();
+    }
+
+    if (gid_file.is_open()) {
+        gid_file << "100" << " " << gid << " 1" << std::endl;
+        gid_file.close();
+    }
+
+    // before this command, lots of things need to be done
     execvp(exe_argv[0], exe_argv);
 
     delete[] exe_argv[0];
     delete[] exe_argv[2];
     delete[] exe_argv;
+
+    return 0;
+
 }
